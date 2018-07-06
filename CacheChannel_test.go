be3443de2626e90a2776ecbbcbe5bb8911dc37e3
石头哥哥 <chenleijava@gmail.com>
2018-07-06 11:16:07 +0800

    Add a hash operation related interface

---
+++ CacheChannel_test.go
@@ -0,0 +1,24 @@
+package go2cache
+
+import (
+	"testing"
+	"time"
+	"log"
+	"github.com/garyburd/redigo/redis"
+)
+
+func TestGetCacheChannel(t *testing.T) {
+	cacheChannel := GetCacheChannel()
+	cache := cacheChannel.GetRedisCache("user_region")
+	var key = time.Now().Format("2006-01-02 15:04:05")
+	var filed = key
+	v := cache.HincrBy(key, filed, 1)
+	log.Printf("hincyBy v:%d", v)
+	hget, _:= redis.Int(cache.Hget(key, filed), nil)
+	log.Printf("Hget v:%d", hget)
+	intMap := cache.HgetAllIntMap(key)
+	log.Printf("HgetAllIntMap:%d", intMap[key])
+	for k := range intMap {
+		delete(intMap, k)
+	}
+}
--- RedisCache.go
+++ RedisCache.go
@@ -5,6 +5,10 @@ import (
 	"log"
 )
 
+//const (
+//	PING, SET, GET, QUIT, EXISTS, DEL, TYPE, FLUSHDB, KEYS, RANDOMKEY, RENAME, RENAMENX, RENAMEX, DBSIZE, EXPIRE, EXPIREAT, TTL, SELECT, MOVE, FLUSHALL, GETSET, MGET, SETNX, SETEX, MSET, MSETNX, DECRBY, DECR, INCRBY, INCR, APPEND, SUBSTR, HSET, HGET, HSETNX, HMSET, HMGET, HINCRBY, HEXISTS, HDEL, HLEN, HKEYS, HVALS, HGETALL, RPUSH, LPUSH, LLEN, LRANGE, LTRIM, LINDEX, LSET, LREM, LPOP, RPOP, RPOPLPUSH, SADD, SMEMBERS, SREM, SPOP, SMOVE, SCARD, SISMEMBER, SINTER, SINTERSTORE, SUNION, SUNIONSTORE, SDIFF, SDIFFSTORE, SRANDMEMBER, ZADD, ZRANGE, ZREM, ZINCRBY, ZRANK, ZREVRANK, ZREVRANGE, ZCARD, ZSCORE, MULTI, DISCARD, EXEC, WATCH, UNWATCH, SORT, BLPOP, BRPOP, AUTH, SUBSCRIBE, PUBLISH, UNSUBSCRIBE, PSUBSCRIBE, PUNSUBSCRIBE, PUBSUB, ZCOUNT, ZRANGEBYSCORE, ZREVRANGEBYSCORE, ZREMRANGEBYRANK, ZREMRANGEBYSCORE, ZUNIONSTORE, ZINTERSTORE, ZLEXCOUNT, ZRANGEBYLEX, ZREVRANGEBYLEX, ZREMRANGEBYLEX, SAVE, BGSAVE, BGREWRITEAOF, LASTSAVE, SHUTDOWN, INFO, MONITOR, SLAVEOF, CONFIG, STRLEN, SYNC, LPUSHX, PERSIST, RPUSHX, ECHO, LINSERT, DEBUG, BRPOPLPUSH, SETBIT, GETBIT, BITPOS, SETRANGE, GETRANGE, EVAL, EVALSHA, SCRIPT, SLOWLOG, OBJECT, BITCOUNT, BITOP, SENTINEL, DUMP, RESTORE, PEXPIRE, PEXPIREAT, PTTL, INCRBYFLOAT, PSETEX, CLIENT, TIME, MIGRATE, HINCRBYFLOAT, SCAN, HSCAN, SSCAN, ZSCAN, WAIT, CLUSTER, ASKING, PFADD, PFCOUNT, PFMERGE, READONLY, GEOADD, GEODIST, GEOHASH, GEOPOS, GEORADIUS, GEORADIUSBYMEMBER, BITFIELD
+//)
+
 //redigo doc
 //https://godoc.org/github.com/garyburd/redigo/redis
 
@@ -22,7 +26,7 @@ func (cache *RedisCache) do(commandName string, args ...interface{}) (reply inte
 }
 
 //获取 cacheObject
-func (cache *RedisCache) Get(key string) *CacheObject {
+func (cache *RedisCache) GetCacheObject(key string) *CacheObject {
 	reply, err := cache.do("GET", key)
 	if err != nil {
 		log.Printf("get bytes with key:%s error:%s ", key, err)
@@ -44,7 +48,7 @@ func (cache *RedisCache) Put(key string, value interface{}) error {
 }
 
 //删除缓存数据
-func (cache *RedisCache) Delete(key string) error {
+func (cache *RedisCache) Del(key string) error {
 	_, err := cache.do("DEL", key)
 	return err
 }
@@ -63,3 +67,39 @@ func (cache *RedisCache) Incr(key string) error {
 	_, err := redis.Bool(cache.do("INCRBY", key, 1))
 	return err
 }
+
+//获取 key 对应的值
+func (cache *RedisCache) Get(key string) interface{} {
+	reply, _ := cache.do("GET", key)
+	return reply
+}
+
+//hincryBy 基于hash 计数
+func (cache *RedisCache) HincrBy(key, filed string, value int) int64 {
+	v, _ := redis.Int64(cache.do("HINCRBY", key, filed, value))
+	return v
+}
+//HGET
+func (cache *RedisCache) Hget(key, filed string) interface{} {
+	reply, _ := cache.do("HGET", key, filed)
+	return reply
+}
+//HGETALL
+func (cache *RedisCache) HgetAllStringMap(key string) map[string]string {
+	mp, err := redis.StringMap(cache.do("HGETALL", key))
+	if err != nil {
+		log.Printf("Hgetall error:%s", err.Error())
+	}
+	return mp
+}
+//HGETALL
+func (cache *RedisCache) HgetAllIntMap(key string) map[string]int {
+	mp, err := redis.IntMap(cache.do("HGETALL", key))
+	if err != nil {
+		log.Printf("Hgetall error:%s", err.Error())
+	}
+	return mp
+}
+
+
+
